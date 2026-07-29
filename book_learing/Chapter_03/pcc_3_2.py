dae6ffe116d8b023c8d64fde556dd3e985d9cf24
Fmaj7124 <729881188@qq.com>
2026-07-29 15:41:48 +0800

    Rename snake_3_2.py to pcc_3_2.py

---
+++ book_learing/Chapter_03/pcc_3_2.py
@@ -0,0 +1,4 @@
+import random
+
+commuting_mode = ['car' , 'plane' , 'boat' , 'bike']
+print(f'I would like to go to school by {random.choice(commuting_mode)}')
--- book_learing/Chapter_03/snake_3_2.py
+++
@@ -1,4 +0,0 @@
-import random
-
-commuting_mode = ['car' , 'plane' , 'boat' , 'bike']
-print(f'I would like to go to school by {random.choice(commuting_mode)}')
