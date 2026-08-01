f7e72e400baaa20dbf8cc50dd95a9420d3a2b5be
Fmaj7124 <729881188@qq.com>
2026-08-01 21:03:14 +0800

    Update pcc_7_4.py

--- book_learing/Chapter_07/pcc_7_4.py
+++ book_learing/Chapter_07/pcc_7_4.py
@@ -3,6 +3,7 @@ prompt = '请输入您想添加的配料: '
 while True:
     topping =input(prompt)
     toppings.append(topping)
-    print(toppings)
     if topping =='quit':
         break
+    toppings.append(topping)
+    print(toppings)
