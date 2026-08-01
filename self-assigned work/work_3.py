3a4761870e4a4334aef540ce00ad9275da5ec45d
Fmaj7124 <729881188@qq.com>
2026-08-01 22:16:01 +0800

    Add files via upload

---
+++ self-assigned work/work_3.py
@@ -0,0 +1,18 @@
+#游乐场门票机
+while True:
+    age = input("\nHow old are you?(Or enter 'quit' to end the program): ")
+    if age == 'quit':
+        break
+    age = int(age)
+    if age < 6:
+        price='free'
+        print(price)
+    elif age < 18:
+        price =20
+        print(f'You need to pay {price}')
+    elif age < 60:
+        price = 50
+        print(f'You need to pay {price}')
+    else:
+        price = 10
+        print(f'You need to pay {price}')
